ae714c56cd4e52f0bd56da62839524fa608ab421
Abdul Irshad <er.abdulirshad@gmail.com>
2020-10-24 13:07:19 +0530

    Added Category Model and Interface

---
+++ OShopAPI/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OShopAPI.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ OShopAPI/Repository/ICategoryRepo.cs
@@ -0,0 +1,11 @@
+﻿using OShopAPI.Models;
+using System.Collections.Generic;
+
+namespace OShopAPI.Repository
+{
+    public  interface ICategoryRepo
+    {
+        IEnumerable<Category> GetAllCategory();
+
+    }
+}
